3226428e7390b884640fa4bad366aa2570fa92ed
goatwu1993 <peteeelol@gmail.com>
2023-04-17 17:54:48 +0800

    refactor: allow passing extra args at init

--- pycobertura_test/add.py
+++ pycobertura_test/add.py
@@ -11,3 +11,14 @@ def not_covered_function_in_main_branch():
 
 def is_even(i: int):
     return i//2 == 0
+
+def is_fruit(s: str):
+    """
+    A multiline uncovered feature
+    """
+    if s == "orange":
+        return True
+    if s == "grape":
+        return True
+    if s == "apple":
+        return True
