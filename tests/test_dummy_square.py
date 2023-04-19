fb1306392787caed25b3796212473cbae5ca955e
goatwu1993 <peteeelol@gmail.com>
2023-04-19 13:26:15 +0800

    chore: drop square 2 test

--- tests/test_dummy_square.py
+++ tests/test_dummy_square.py
@@ -3,6 +3,6 @@ import pytest
 from pycobertura_test.dummy_square import dummy_square
 
 
-@pytest.mark.parametrize("i, expected", [(1, 1), (2, 4)])
+@pytest.mark.parametrize("i, expected", [(1, 1)])
 def test_dummy_square(i, expected):
     assert dummy_square(i) == expected
